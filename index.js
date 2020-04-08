56af20124d4b2da0f3be83e01cd0a9c395310337
AdrianDucao <adrianducao@gmail.com>
2020-04-08 23:43:45 +0800

    some update

---
+++ index.js
@@ -0,0 +1,50 @@
+/*
+        JS Resfresher
+    */
+   console.log("Hellow World!"); //printing hello world in console
+
+   /*
+       Data Types
+       undefined, null, boolean, string, symbol, number, and object
+   */
+   var name = "Ian"; //var can be used through whole program
+
+   num = 123;
+
+   let ourName = "motherfuckers"; //within the
+
+   const pi = 3.1415; //constant value that never change
+
+   /*
+       Storing Values with Assignment Operator
+   */
+   var a; //declaring variable
+   var b = 69; //assigning variable
+
+   a = 7; //assigning value
+
+   b = a; //passing value
+
+   console.log(a);
+
+   /*
+       Initializing Variables w/ Assignment Operator
+   */
+   var a = 9;
+
+   /*
+       Uninitialized Variables
+   */
+   var a = 5;
+   var b = 10;
+   var c = "fuck";
+   console.log("Initialized:", a,b,c)
+
+   a = a+5;
+   b = b+5;
+   c = c+" you!";
+   console.log("Uninitialized:", a,b,c)
+
+   /*
+       case sensitivity in variables
+   */
